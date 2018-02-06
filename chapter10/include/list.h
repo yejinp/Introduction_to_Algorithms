935a25c632922b3a72b83fa140c9ec086dc2373a
yejinp <ye895937@126.com>
2018-02-07 07:57:15 +0800

    modify makefile

--- chapter10/include/list.h
+++ chapter10/include/list.h
@@ -1,6 +1,9 @@
 #ifndef _LIST_H_
 #define _LIST_H_
 
+#include <stdlib.h>
+#include <stdio.h>
+
 struct single_list{
     int key;
     struct single_list *next;
@@ -24,6 +27,6 @@ struct single_list * single_list_create();
 void single_list_free(struct single_list *s);
 
 struct double_list * double_list_create();
-void double_list_free(struct single_list *s);
+void double_list_free(struct double_list *d);
 
 #endif
