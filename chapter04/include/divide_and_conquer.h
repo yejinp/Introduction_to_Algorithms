0e656a58590907e52a1569dd43f43f8f49d1d635
yejinp <ye895937@126.com>
2018-03-16 21:22:27 +0800

    add header file

--- chapter04/include/divide_and_conquer.h
+++ chapter04/include/divide_and_conquer.h
@@ -1,7 +1,14 @@
 #ifndef _DIVIDE_AND_CONQUER_H_
 #define _DIVIDE_AND_CONQUER_H_
 
+struct max_array
+{
+	int	first;
+	int	last;
+	int	sum;
+};
 
-
+void find_max_crossing_subarray(int A, int low, int mid, int high);
+void find_maximum_subarray(int A, int low, int high);
 
 #endif
